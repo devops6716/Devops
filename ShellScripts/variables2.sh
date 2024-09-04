e7d62a345f2e48303a1a076f2c6f5aa0f63da8f7
karthik <karthik.nangunuri377@gmail.com>
2024-09-04 17:32:43 +0530

    added variable files

---
+++ ShellScripts/variables2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+#passing variable values at argument but the text of variable while entering is not displayed on the terminal
+
+echo " Enter you username"
+read -s USERNAME 
+echo "Enter your password"
+read -s PASSWORD
+echo " username is :: $USERNAME and password is $PASSWORD"
