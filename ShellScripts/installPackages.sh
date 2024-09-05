6c53da8ac8814914f3c3c6b9d18d35495c618ea5
karthik <karthik.nangunuri377@gmail.com>
2024-09-05 17:01:14 +0530

    sql updated

--- ShellScripts/installPackages.sh
+++ ShellScripts/installPackages.sh
@@ -17,5 +17,13 @@ then
     exit 1
 else
     echo " Installation success"
-        
+    
+yum install git -y   
+# check if installed or not 
+if [ $? -ne 0]
+then
+    echo " Installation failed"
+    exit 1
+else
+    echo " Installation success"      
 
