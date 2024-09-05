e695339b1fe5696c18b65efcc5248af8f4685cdf
karthik <karthik.nangunuri377@gmail.com>
2024-09-05 17:18:26 +0530

    aaded functions

---
+++ functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ID=$(id -u)
+VALIDATE(){
+    if [ $? -ne 0]
+then
+    echo " Installation failed"
+    exit 1
+else
+    echo " Installation success"      
+fi
+}
+if [ $ID -ne 0 ]
+then
+    echo " Error.... Please run with root access"
+    exit 1
+else
+    echo " You are root"
+fi
+
+yum install -y 
+VALIDATE
+yum install git -y
+VALIDATE
