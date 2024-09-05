80184e641ed788853d7462d07c8d18da4b5cae80
karthik <karthik.nangunuri377@gmail.com>
2024-09-05 16:37:02 +0530

    added if condition

---
+++ ShellScripts/conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# if condition to check entered number is greater than 100
+
+if [ $NUMBER -gt 100 ] && [ $NUMBER -lt 200 ]
+then
+    echo "$NUMBER is greater than 100"
+else
+    echo "$NUMBER is lesser than 100"  
+fi      
