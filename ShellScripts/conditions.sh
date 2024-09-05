105d89e93ef61fa203209eb524be6e98ac73216f
karthik <karthik.nangunuri377@gmail.com>
2024-09-05 16:47:03 +0530

    modified if

--- ShellScripts/conditions.sh
+++ ShellScripts/conditions.sh
@@ -4,9 +4,12 @@ NUMBER=$1
 
 # if condition to check entered number is greater than 100
 
+# one way to use && 
+#if [[ $NUMBER -gt 100  && $NUMBER -lt 200 ]] 
 if [ $NUMBER -gt 100 ] && [ $NUMBER -lt 200 ]
 then
     echo "$NUMBER is greater than 100"
 else
     echo "$NUMBER is lesser than 100"  
 fi      
+
