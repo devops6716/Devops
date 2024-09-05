d191b434819f3085ce60a39d5064f785d90df3c6
karthik <karthik.nangunuri377@gmail.com>
2024-09-05 18:07:53 +0530

    updated functions

--- ShellScripts/logs.sh
+++ ShellScripts/logs.sh
@@ -3,18 +3,22 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[om"
 VALIDATE(){
     if [ $1 -ne 0 ]
 then
-    echo " Error.... $2 failed" 
+    echo -e " $R Error.... $2 failed $N" 
     exit 1
 else
-    echo " $2  success"      
+    echo -e " $G $2  success $N"      
 fi
 }
 if [ $ID -ne 0 ]
 then
-    echo " Error.... Please run with root access"
+    echo -e " $R Error.... Please run with root access $N"
     exit 1
 else
     echo " You are root"
