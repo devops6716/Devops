a0bfeb6e1c613617471204a4381ff1ad1ee43701
karthik <karthik.nangunuri377@gmail.com>
2024-09-05 18:55:49 +0530

    modified loops

--- ShellScripts/loops.sh
+++ ShellScripts/loops.sh
@@ -1,5 +1,39 @@
 #!/bin/bash 
-for i in {1..1000}
-do    
-    echo "$i"
-done
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOG_FILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+echo " Script started at $TIMESTAMP" &>>$LOG_FILE
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+    echo -e "  Error.... $2  $R failed $N" 
+    exit 1
+else
+    echo -e "  $2 $G success $N"      
+fi
+}
+if [ $ID -ne 0 ]
+then
+    echo -e " $R Error.... Please run with root access $N"
+    exit 1
+else
+    echo " You are root"
+fi
+
+for PACKAGE in $@
+do 
+    yum list installed $PACKAGE 
+    if [ $? -ne 0 ]
+    then    
+        yum install $PACKAGE -y &>>$LOG_FILE
+        VALIDATE $? " installation of $PACKAGE"
+    else
+        echo "$PACKAGE is already installed"
+    fi
+done            
+
